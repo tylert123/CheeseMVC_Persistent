207a00a52de1c2bd39e8109644cbba356e82bb6f
Tyler Torres <tyler@webangel.net>
2019-11-15 11:38:51 -0500

    Fully functioning CheeseMVC application

---
+++ src/CheeseMVC/Models/Cheese.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class Cheese
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CheeseId { get; set; }
+        private static int nextId = 1;
+
+        /* Below not needed with short hand used in Controller
+        public Cheese(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }*/
+
+        public Cheese() {
+            CheeseId = nextId;
+            nextId++;
+        }
+    }
+}
